16bb63e0c093d79addf8c8dfcfbfe204218cff89
mohmed2000 <noza25588@gmail.com>
2021-11-23 16:20:57 +0200

    Fill (Patient List) with data from array, create function (renderTable)_6

--- app/main.js
+++ app/main.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
   $(".action-link").click(onActionLinkClick);
+  renderTable();
 });
 
 function onActionLinkClick(e) {
@@ -9,4 +10,25 @@ function onActionLinkClick(e) {
 function navigate(dataComponent) {
   $(".component").hide();
   $(`.${dataComponent}`).show();
+}
+function renderTable() {
+  patientsData.forEach((data) => {
+    let RenderTemplate = `
+    <tr class="table-success">
+    <th scope="row">${data.ID}</th>
+    <td> ${data.fname} ${data.mname} ${data.lname} </td>
+    <td>${data.email}</td>
+    <td>${data.gender}</td>
+    <td>${data.DOB}</td>
+    <td>${data.status}</td>
+    <td>${data.CreatedBy} at ${data.creationDate}</td>
+    <td>
+      <button class="btn btn-primary" data-target="patient-edit">
+        Edit
+      </button>
+    </td>
+  </tr>
+  `;
+    $(".patient-list-body").append(RenderTemplate);
+  });
 }

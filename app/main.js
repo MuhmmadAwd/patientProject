34f9afd523578df3a2ac516506f84850afdc55cc
mohmed2000 <noza25588@gmail.com>
2021-11-23 16:59:54 +0200

    Templating :: Create (renderTemplate) function

--- app/main.js
+++ app/main.js
@@ -12,23 +12,29 @@ function navigate(dataComponent) {
   $(`.${dataComponent}`).show();
 }
 function renderTable() {
+  let templateText = $("#patient-list-template").html();
+
   patientsData.forEach((data) => {
-    let RenderTemplate = `
-    <tr class="table-success">
-    <th scope="row">${data.ID}</th>
-    <td> ${data.fname} ${data.mname} ${data.lname} </td>
-    <td>${data.email}</td>
-    <td>${data.gender}</td>
-    <td>${data.DOB}</td>
-    <td>${data.status}</td>
-    <td>${data.CreatedBy} at ${data.creationDate}</td>
-    <td>
-      <button class="btn btn-primary" data-target="patient-edit">
-        Edit
-      </button>
-    </td>
-  </tr>
-  `;
+    let RenderTemplate = Rendertemplate(templateText, data);
+
     $(".patient-list-body").append(RenderTemplate);
   });
 }
+
+Rendertemplate = (templateText, data) => {
+  for (let i = 0; i < 10; i++) {
+    let fisrtBraces = templateText.search("{{");
+    let lastBraces = templateText.search("}}");
+    let propertyWithBreaces = templateText.slice(fisrtBraces, lastBraces + 2);
+    let property = templateText.slice(fisrtBraces + 2, lastBraces);
+    let propertyData = data[property];
+    let editTemplateText = templateText.replace(
+      propertyWithBreaces,
+      propertyData
+    );
+    let orignalTemplateText = templateText;
+    templateText = editTemplateText;
+    orignalTemplateText = editTemplateText;
+  }
+  return templateText;
+};

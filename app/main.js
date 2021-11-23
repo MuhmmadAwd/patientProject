765603366858529442c3be344e54ede6dde99b0b
mohmed2000 <noza25588@gmail.com>
2021-11-23 15:30:30 +0200

    Routing Finalize (routing)_5

--- app/main.js
+++ app/main.js
@@ -1,18 +1,12 @@
 $(document).ready(function () {
-  $(".patients").click(onRouting);
-  $(".users").click(onRouting);
-  $(".add-patient").click(onRouting);
-  $(".edit-patient").click(onRouting);
-  $(".add-users").click(onRouting);
-  $(".edit-users").click(onRouting);
-  $(".save-users").click(onRouting);
-  $(".delete-users").click(onRouting);
-  $(".save-patient").click(onRouting);
-  $(".delete-patient").click(onRouting);
+  $(".action-link").click(onActionLinkClick);
 });
 
-function onRouting(e) {
-  let page = $(e.target).data("routing");
-  $(".route").hide();
-  $(`.${page}`).show();
+function onActionLinkClick(e) {
+  let dataComponent = $(e.target).data("target");
+  navigate(dataComponent);
+}
+function navigate(dataComponent) {
+  $(".component").hide();
+  $(`.${dataComponent}`).show();
 }
